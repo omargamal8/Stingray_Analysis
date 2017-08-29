d29b428169dc08646e873eb39358cacdd5d98a65
omargamal8 <omargamalh@hotmail.com>
2017-08-29 16:16:04 +0200

    re activated the run all tests

--- RunAllTests.py
+++ RunAllTests.py
@@ -21,7 +21,7 @@ def findSingleTest(dir,depth=0, keyword = "SingleTest.py"):
 if __name__ == '__main__':
 	current_dir = os.getcwd()
 	# current_dir +='/Lightcurve'
-	modules = [dir for dir in os.listdir(current_dir) if os.path.isdir(current_dir+'/'+dir) if not dir == ".git" if not dir == "__pycache__"]
+	modules = [dir for dir in os.listdir(current_dir) if os.path.isdir(current_dir+'/'+dir) if not dir == ".git" if not dir == "__pycache__" if not "Summary" in dir]
 	# modules = [modules[0]]
 	print(modules)
 	# print(modules)
@@ -30,16 +30,15 @@ if __name__ == '__main__':
 			print("Module",module)
 			module_fullpath = current_dir + '/' + module + '/'
 			SingleTests = findSingleTest(module_fullpath)
-			SingleTests = [module_fullpath+'coherence/Static_SegmentSize/',module_fullpath+'time_lag/Static_SegmentSize/']
 			if SingleTests == None or SingleTests==[]:
 				print("SingleTest.py not found in " + module)
 				continue
 			else:
 				print(SingleTests)
-				# for SingleTest in SingleTests:
-				# 	print("Testing "+ SingleTest)
-				# 	Tester = ContinuousTester(4,4,9)
-				# 	Tester.testAndWrite(SingleTest)
-					# plotter.plotAndSaveFromRaw(SingleTest+"/TimeProfiles/plotting_values.txt")
+				for SingleTest in SingleTests:
+					print("Testing "+ SingleTest)
+					Tester = ContinuousTester(4,4,9)
+					Tester.testAndWrite(SingleTest)
+					plotter.plotAndSaveFromRaw(SingleTest+"/TimeProfiles/plotting_values.txt")
 			print("Plots in Module")
 			plotter.plotGroup(module_fullpath)
